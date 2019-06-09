84c50491e853af90432e4ef53ebb514a39f8ad7d
kplunkett512itk <42696964+kplunkett512itk@users.noreply.github.com>
2019-06-08 23:53:06 -0500

    Add files via upload

---
+++ resource-group/Resource_Groups_v1.ps1
@@ -0,0 +1,53 @@
+﻿### Login to Azure and Set the Subscription
+Connect-AzureRmAccount
+Select-AzureRMSubscription -Subscription 48db8903-41b0-4a8d-9a07-62f6b7a7d515
+
+### Define Deployment Variables
+{
+$location = 'West Europe'
+$resourceGroupNameNetwork = 'rg_6182012_net'
+$resourceGroupNameProduction = 'rg_6182012_prod'
+$resourceGroupNameStage = 'rg_6182012_stage'
+$resourceGroupNameTest = 'rg_6182012_test'
+$resourceGroupNameDev = 'rg_6182012_dev'
+}
+
+### Create Resource Group for Network
+{
+New-AzureRmResourceGroup `
+    -Name $resourceGroupNameNetwork `
+    -Location $location `
+    -Verbose -Force
+}
+
+### Create Resource Group for Production
+{
+New-AzureRmResourceGroup `
+    -Name $resourceGroupNameProduction `
+    -Location $location `
+    -Verbose -Force
+}
+
+### Create Resource Group for Stage
+{
+New-AzureRmResourceGroup `
+    -Name $resourceGroupNameStage `
+    -Location $location `
+    -Verbose -Force
+}
+
+### Create Resource Group for Test
+{
+New-AzureRmResourceGroup `
+    -Name $resourceGroupNameTest `
+    -Location $location `
+    -Verbose -Force
+}
+
+### Create Resource Group for Dev
+{
+New-AzureRmResourceGroup `
+    -Name $resourceGroupNameDev `
+    -Location $location `
+    -Verbose -Force
+}
